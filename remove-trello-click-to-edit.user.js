e80be3731a49c7b4130eeef0b39b51509b641337
Dan Bozaru <dan@bozaru.ro>
2019-12-10 10:40:25 +0200

    Create remove-trello-click-to-edit.user.js

---
+++ remove-trello-click-to-edit.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name          Remove editability on click from trello card details
+// @namespace     https://github.com/danbozaru
+// @version       1.0.0
+// @downloadURL   https://github.com/danbozaru/tampermonkey/blob/master/remove-trello-click-to-edit.user.js
+// @description   Removes click to edit behacvior from card detail descriptions within modals.
+// @author        danbozaru
+// @include       https://trello.com/*
+// @run-at        document-start
+// @grant         GM_log
+// ==/UserScript==
+
+(function(win) {
+    'use strict';
+
+    var listeners = [],
+        doc = win.document,
+        MutationObserver = win.MutationObserver || win.WebKitMutationObserver,
+        observer;
+
+    function ready(selector, fn) {
+        // Store the selector and callback to be monitored
+        listeners.push({
+            selector: selector,
+            fn: fn
+        });
+        if (!observer) {
+            // Watch for changes in the document
+            observer = new MutationObserver(check);
+            observer.observe(doc.documentElement, {
+                childList: true,
+                subtree: true
+            });
+        }
+        // Check if the element is currently in the DOM
+        check();
+    }
+
+    function check() {
+        // Check the DOM for elements matching a stored selector
+        for (var i = 0, len = listeners.length, listener, elements; i < len; i++) {
+            listener = listeners[i];
+            // Query for elements matching the specified selector
+            elements = doc.querySelectorAll(listener.selector);
+            for (var j = 0, jLen = elements.length, element; j < jLen; j++) {
+                element = elements[j];
+                // Make sure the callback isn't invoked with the
+                // same element more than once
+                if (!element.ready) {
+                    element.ready = true;
+                    // Invoke the callback with the element
+                    listener.fn.call(element, element);
+                }
+            }
+        }
+    }
+
+    // Expose `ready`
+    win.ready = ready;
+})(this);
+
+window.ready('.window-module .editable', node => {
+    node.classList.remove('editable');
+});
